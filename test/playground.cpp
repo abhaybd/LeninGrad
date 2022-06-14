e3a14e0c195c411594b3c6cd905029d70a3b1b2c
Abhay D <a.deshpande012@gmail.com>
2022-06-13 21:14:22 -0700

    Remove playground

--- test/playground.cpp
+++
@@ -1,8 +0,0 @@
-#include <type_traits>
-#include <iostream>
-#include <complex>
-
-int main() {
-    std::cout << "double: " << std::is_floating_point<double>::value;
-    std::cout << "complex: " << std::is_floating_point<std::complex<double>>::value;
-}
